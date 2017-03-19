f2f67572daa65431a6172387a170ae4aad30e18c
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-03-19 20:32:49 +0100

    add consent, lng picker, localization

--- js/main.js
+++ js/main.js
@@ -1,3 +1,56 @@
+// localization
+var ele = document.getElementById('languageSelect');
+var availableLngs = [];
+
+// create select options based on project languages
+locizify.getLanguages(function(err, lngs) {
+  availableLngs = Object.keys(lngs || {});
+  availableLngs.forEach(function(l) {
+    var lng = lngs[l];
+    if (lng.translated.production < 0.9) return;
+    var optEle = document.createElement("OPTION");
+    optEle.setAttribute('value', l);
+    optEle.innerHTML = l; //lng.nativeName;
+    ele.appendChild(optEle);
+  });
+
+  updateSelect();
+});
+
+function updateSelect() {
+  var selected;
+  locizify.i18next.languages.forEach(function(l) {
+    if (!selected && availableLngs.indexOf(l) > -1) selected = l;
+  });
+
+  ele.value = selected || 'en';
+}
+
+locizify.i18next.on('languageChanged', function(lng) {
+  updateSelect();
+});
+
+function handleSelectChange() {
+  var value = ele.options[ele.selectedIndex].value;
+  changeLanguage(value);
+}
+
+function updateQueryStringParameter(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    return uri + separator + key + "=" + value;
+  }
+}
+function changeLanguage(lng) {
+  window.location = updateQueryStringParameter(window.location.href, 'lng', lng);
+}
+
+
+
 (function($) {
     "use strict"; // Start of use strict
 
@@ -28,116 +81,117 @@
         }
     })
 
+
     // particles
     particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 20,
-      "density": {
-        "enable": false,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#2196f3"
-    },
-    "shape": {
-      "type": "polygon",
-      "stroke": {
-        "width": 0,
-        "color": "#000"
-      },
-      "polygon": {
-        "nb_sides": 6
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.09620472365193136,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 120.2559045649142,
-      "random": false,
-      "anim": {
-        "enable": true,
-        "speed": 10,
-        "size_min": 40,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 200,
-      "color": "#ffffff",
-      "opacity": 1,
-      "width": 2
-    },
-    "move": {
-      "enable": true,
-      "speed": 1,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
+      "particles": {
+        "number": {
+          "value": 20,
+          "density": {
+            "enable": false,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#2196f3"
+        },
+        "shape": {
+          "type": "polygon",
+          "stroke": {
+            "width": 0,
+            "color": "#000"
+          },
+          "polygon": {
+            "nb_sides": 6
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.09620472365193136,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 120.2559045649142,
+          "random": false,
+          "anim": {
+            "enable": true,
+            "speed": 10,
+            "size_min": 40,
+            "sync": false
+          }
+        },
         "line_linked": {
-          "opacity": 1
+          "enable": false,
+          "distance": 200,
+          "color": "#ffffff",
+          "opacity": 1,
+          "width": 2
+        },
+        "move": {
+          "enable": true,
+          "speed": 1,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
         }
       },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": false,
+            "mode": "grab"
+          },
+          "onclick": {
+            "enable": false,
+            "mode": "push"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 200,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
       },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
+      "retina_detect": true
+    });
 
 })(jQuery); // End of use strict
